f020cf0ce85b8739cac5c239c50697cebeb0da71
ELCOMANDANTE18 <vb.gimenez@alumno.um.edu.ar>
2025-11-07 18:36:30 -0300

    pwa

---
+++ pwa-angular/pwa-angular/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { PersonsComponent } from './persons/persons.component';
+import { ContactsComponent } from './contacts/contacts.component';
+
+export const routes: Routes = [
+  { path: 'users', component: UsersComponent },
+  { path: 'persons', component: PersonsComponent },
+  { path: 'contacts', component: ContactsComponent },
+  { path: '', redirectTo: 'users', pathMatch: 'full' } // Redirige a /users por defecto
+];
